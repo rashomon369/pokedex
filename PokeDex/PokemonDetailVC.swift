d012fcdcc7020467bb3975c7089a2dffa8416a13
rashomon369 <wang39@gmail.com>
2016-12-24 12:57:05 -0800

    Added UI layout for displaying pokemon details

--- PokeDex/PokemonDetailVC.swift
+++ PokeDex/PokemonDetailVC.swift
@@ -10,15 +10,13 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
 
-    @IBOutlet weak var pokemonNameLabel: UILabel!
-    
     var pokemon: Pokemon!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       pokemonNameLabel.text = pokemon.name
+       
     }
 
  
